99870a80995d49defe56068477870f5678992660
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-01-05 09:23:23 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,26 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Các số nguyên tố nhỏ hơn 100: ");
+        for (int number = 2; number <100; number ++){
+            if (number == 2){
+                System.out.println(number);
+            }
+            else{
+                boolean isPrime = true;
+                for(int i = 2; i <number;i++){
+                    if (number % i == 0 ){
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime){
+                    System.out.println(number);
+                }
+            }
+        }
+    }
+}
